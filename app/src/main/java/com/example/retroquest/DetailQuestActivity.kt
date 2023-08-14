3f1939e85c3a89f14818eacc6ae0bbdb197a6203
bora park <jemini9595@gmail.com>
2023-08-14 18:58:21 +0900

    유저페이지 ⚙️

---
+++ app/src/main/java/com/example/retroquest/DetailQuestActivity.kt
@@ -0,0 +1,11 @@
+package com.example.retroquest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailQuestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_quest)
+    }
+}
