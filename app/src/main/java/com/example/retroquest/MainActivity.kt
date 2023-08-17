fffef2e432eecfe9fc5b9259b5c1c4368c481bbf
tastystar97 <63907062+tastystar97@users.noreply.github.com>
2023-08-17 15:40:03 +0900

    오류 수정 : 🔧

--- app/src/main/java/com/example/retroquest/MainActivity.kt
+++ app/src/main/java/com/example/retroquest/MainActivity.kt
@@ -1,27 +1,14 @@
 package com.example.retroquest
 
 
-import android.app.Activity
 import android.content.Intent
-import android.icu.text.SimpleDateFormat
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.view.LayoutInflater
-import android.view.View
-import android.view.animation.Animation
-import android.view.animation.AnimationUtils
 import android.widget.Button
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.TextView
-import pl.droidsonroids.gif.GifDrawable
-import java.util.Date
-import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
 
-    private val questList = mutableListOf<questData>()
+    private val questList = mutableListOf<QuestData>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
--- app/src/main/java/com/example/retroquest/questData.kt
+++
@@ -1,3 +0,0 @@
-package com.example.retroquest
-
-data class questData(var questName:String, var questDes:String, var questTypes:String)
