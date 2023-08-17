69d44a5c279c6057521230a4b1108ed7d94bcf5e
tastystar97 <63907062+tastystar97@users.noreply.github.com>
2023-08-17 09:57:19 +0900

    ui 버튼 추가, 임시 저장 : 🔧

--- app/src/main/java/com/example/retroquest/MainActivity.kt
+++ app/src/main/java/com/example/retroquest/MainActivity.kt
@@ -1,17 +1,32 @@
 package com.example.retroquest
 
+
+import android.app.Activity
 import android.content.Intent
+import android.icu.text.SimpleDateFormat
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.DynamicLayout
-import android.widget.ArrayAdapter
+import android.os.Handler
+import android.view.LayoutInflater
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.Button
-import androidx.recyclerview.widget.RecyclerView
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import pl.droidsonroids.gif.GifDrawable
+import java.util.Date
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        private val questList = mutableListOf<questData>()
+
+
 
 
         var detailQuest = findViewById<Button>(R.id.questBtn)
@@ -25,8 +40,9 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this,UserListActivity::class.java)
             startActivity(intent)
         }
-
-        }
     }
+
+
+}
 
 
