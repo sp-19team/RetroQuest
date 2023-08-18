521a955a6268a94b7e83c30cfbc4c8328d8b374b
Jun Beom Park <rheo191710@gmail.com>
2023-08-18 11:19:31 +0900

    [FEAT] : ⚙️ DetailQuest 기능추가 [DESIGN] : 🎨 DetailQuest UI 수정

--- app/src/main/java/com/example/retroquest/QuestData.kt
+++ app/src/main/java/com/example/retroquest/QuestData.kt
@@ -1,3 +1,32 @@
 package com.example.retroquest
 
-data class QuestData(var title: String, var des: String, var type: String)
+import android.os.Parcel
+import android.os.Parcelable
+
+data class QuestData(var title: String, var des: String, var type: String) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readString() ?: ""
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(title)
+        parcel.writeString(des)
+        parcel.writeString(type)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<QuestData> {
+        override fun createFromParcel(parcel: Parcel): QuestData {
+            return QuestData(parcel)
+        }
+
+        override fun newArray(size: Int): Array<QuestData?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
