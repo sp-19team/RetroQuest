97371690807d27aec9b335977856520c04d91f60
tastystar97 <63907062+tastystar97@users.noreply.github.com>
2023-08-19 17:34:34 +0900

    [FIX] : 🔧 메인 액티비티 수정
    로그인 액티비티에 들어간 메인 액티비티를 다시 수정했습니다.

--- app/src/main/java/com/example/retroquest/LoginActivity.kt
+++ app/src/main/java/com/example/retroquest/LoginActivity.kt
@@ -1,185 +1,92 @@
 package com.example.retroquest
 
-
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import pl.droidsonroids.gif.GifDrawable
 
-class MainActivity : AppCompatActivity() {
-
-    private val questList = mutableListOf<QuestData>()
+class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
 
 
-        var community = findViewById<Button>(R.id.questBtn)
-        community.setOnClickListener {
-            val intent = Intent(this, ComunityActivity::class.java)
-            startActivity(intent)
+        val loginText = findViewById<EditText>(R.id.loginEditTextId)
+        val pwText = findViewById<EditText>(R.id.loginEditTextPw)
+        val mainImg = findViewById<ImageView>(R.id.profileImages)
+
+
+        val gifImage =findViewById<ImageView>(R.id.profileImages)
+        val gifDrawable = GifDrawable(resources,R.drawable.movecat)
+        gifImage.setImageDrawable(gifDrawable)
+        gifDrawable.start()
+
+
+
+        //signUp에서 정보 가져오기
+        val activityResultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                if (it.resultCode == RESULT_OK) {
+                    val user_id = it.data?.getStringExtra("id") ?: ""
+                    val user_pw = it.data?.getStringExtra("pw") ?: ""
+                    val user_img = it.data?.getStringExtra("img") ?: "0"
+
+
+                    mainImg.setImageResource(when(user_img.toInt()){
+                        1->R.drawable.cuvi
+                        2->R.drawable.mario
+                        3->R.drawable.sonic1
+                        4->R.drawable.sonic2
+                        else -> R.drawable.cuvi
+                    })
+
+                    loginText.setText(user_id)
+                    pwText.setText(user_pw)
+
+                }
+            }
+
+        // 시작 버튼 눌렸을 때 main화면 실행
+        val login = findViewById<Button>(R.id.loginBtn)
+
+        login.setOnClickListener {
+            val loginId = loginText.text.toString()
+            val loginPw = pwText.text.toString()
+            if (loginId.isNotEmpty() && loginPw.isNotEmpty()){
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+
+            }else {
+                Toast.makeText(this, "입력되지 않은 정보가 있습니다.", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+        // 회원가입 버튼 눌렸을 때 signUp화면 실행
+
+        val signUp = findViewById<Button>(R.id.signUpBtn)
+        signUp.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            activityResultLauncher.launch(intent)
             overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
-        }
 
-        var userlList = findViewById<Button>(R.id.userListBtn)
-        userlList.setOnClickListener {
-            val intent = Intent(this, UserListActivity::class.java)
-            startActivity(intent)
-            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
-        }
-
-        val quest1 = QuestData("알고리즘 테스트", "lv.1 알고리즘 문제를 3회 풉니다.", "일일 퀘스트")
-        val quest2 = QuestData("팀 프로젝트", "팀 프로젝트를 완성합니다.", "주간 퀘스트")
-        val quest3 = QuestData("TIL 작성", "TIL을 작성합니다.", "일일 퀘스트")
-        val quest4 = QuestData("WIL 작성", "WIL을 작성합니다.", "주간 퀘스트")
-        val quest5 = QuestData("코틀린 문법 학습", "코틀린 문법을 공부합니다.", "일일 퀘스트")
-
-
-        val isQuestCompleted1 = intent.getBooleanExtra("QUEST_COMPLETED1", false)
-        val imageView = findViewById<ImageView>(R.id.scroll1)
-
-        if (isQuestCompleted1) {
-            imageView.setBackgroundResource(R.drawable.clear)
-        } else {
-            imageView.setBackgroundResource(R.drawable.quest3)
         }
 
 
-        val questbtn1 = findViewById<Button>(R.id.quest1)
-        questbtn1.setOnClickListener {
-            val intent = Intent(this, DetailQuestActivity::class.java)
-            intent.putExtra("QUESTDATA", quest1)
-            startActivity(intent)
-            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
-        }
-
-        val isQuestCompleted2 = intent.getBooleanExtra("QUEST_COMPLETED2", false)
-        val imageView = findViewById<ImageView>(R.id.scroll1)
-
-        if (isQuestCompleted2) {
-            imageView.setBackgroundResource(R.drawable.clear)
-        } else {
-            imageView.setBackgroundResource(R.drawable.quest3)
-        }
-
-        val questbtn2 = findViewById<Button>(R.id.quest2)
-        questbtn2.setOnClickListener {
-            val intent = Intent(this, DetailQuestActivity::class.java)
-            intent.putExtra("QUESTDATA", quest2)
-            startActivity(intent)
-            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
-        }
-
-        val isQuestCompleted3 = intent.getBooleanExtra("QUEST_COMPLETED3", false)
-        val imageView = findViewById<ImageView>(R.id.scroll1)
-
-        if (isQuestCompleted3) {
-            imageView.setBackgroundResource(R.drawable.clear)
-        } else {
-            imageView.setBackgroundResource(R.drawable.quest3)
-        }
-
-        val questbtn3 = findViewById<Button>(R.id.quest3)
-        questbtn3.setOnClickListener {
-            val intent = Intent(this, DetailQuestActivity::class.java)
-            intent.putExtra("QUESTDATA", quest3)
-            startActivity(intent)
-            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
-        }
-
-        val isQuestCompleted4 = intent.getBooleanExtra("QUEST_COMPLETED4", false)
-        val imageView = findViewById<ImageView>(R.id.scroll1)
-
-        if (isQuestCompleted4) {
-            imageView.setBackgroundResource(R.drawable.clear)
-        } else {
-            imageView.setBackgroundResource(R.drawable.quest3)
-        }
-
-        val questbtn4 = findViewById<Button>(R.id.quest4)
-        questbtn4.setOnClickListener {
-            val intent = Intent(this, DetailQuestActivity::class.java)
-            intent.putExtra("QUESTDATA", quest4)
-            startActivity(intent)
-            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
-        }
-
-        val isQuestCompleted4 = intent.getBooleanExtra("QUEST_COMPLETED4", false)
-        val imageView = findViewById<ImageView>(R.id.scroll1)
-
-        if (isQuestCompleted4) {
-            imageView.setBackgroundResource(R.drawable.clear)
-        } else {
-            imageView.setBackgroundResource(R.drawable.quest3)
-        }
-
-        val isQuestCompleted4 = intent.getBooleanExtra("QUEST_COMPLETED4", false)
-        val imageView = findViewById<ImageView>(R.id.scroll1)
-
-        if (isQuestCompleted4) {
-            imageView.setBackgroundResource(R.drawable.clear)
-        } else {
-            imageView.setBackgroundResource(R.drawable.quest3)
-        }
-
-        val questbtn5 = findViewById<Button>(R.id.quest5)
-        questbtn5.setOnClickListener {
-            val intent = Intent(this, DetailQuestActivity::class.java)
-            intent.putExtra("QUESTDATA", quest5)
-            startActivity(intent)
-            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
-        }
-
-        val reward1 = ResultData("알고리즘 +1" ,"자신감 +1","실력 +1")
-        val reward2 = ResultData("협동력 +1" ,"자신감 +1","실력 +1")
-        val reward3 = ResultData("성실함 +1" ,"자신감 +1","실력 +1")
-        val reward4 = ResultData("성실함 +1" ,"자신감 +1","실력 +1")
-        val reward5 = ResultData("코틀린 +1" ,"자신감 +1","실력 +1")
-
-
-// 보상 선택시 액티비티 전환
-        val resultbtn1 = findViewById<Button>(R.id.questResult1)
-        resultbtn1.setOnClickListener {
-            val intent = Intent(this, ResultActivity::class.java)
-            intent.putExtra("RESULTDATA", reward1)
-            startActivity(intent)
-            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
-        }
-
-        val resultbtn2 = findViewById<Button>(R.id.questResult2)
-        resultbtn2.setOnClickListener {
-            val intent = Intent(this, ResultActivity::class.java)
-            intent.putExtra("RESULTDATA", reward2)
-            startActivity(intent)
-            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
-        }
-
-        val resultbtn3 = findViewById<Button>(R.id.questResult3)
-        resultbtn3.setOnClickListener {
-            val intent = Intent(this, ResultActivity::class.java)
-            intent.putExtra("RESULTDATA", reward3)
-            startActivity(intent)
-            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
-        }
-
-        val resultbtn4 = findViewById<Button>(R.id.questResult4)
-        resultbtn4.setOnClickListener {
-            val intent = Intent(this, ResultActivity::class.java)
-            intent.putExtra("RESULTDATA", reward4)
-            startActivity(intent)
-            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
-        }
-
-        val resultbtn5 = findViewById<Button>(R.id.questResult5)
-        resultbtn5.setOnClickListener {
-            val intent = Intent(this, ResultActivity::class.java)
-            intent.putExtra("RESULTDATA", reward5)
-            startActivity(intent)
-            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
-        }
     }
+
+
 }
+
+
 
